df08207b205f3edd8710f5255447526e05c250b0
Kelly Leppik <kelllep@greeny.cs.tlu.ee>
2015-09-07 14:13:39 +0300

    copied original

---
+++ helloworld.php
@@ -0,0 +1,25 @@
+<?php
+
+	$first_name = "Romil";
+	$last_name = "Rõbtðenkov";
+
+	//trükib välja
+	echo $first_name." ".$last_name;
+
+?>
+
+<br>
+
+<?php 
+	
+	$age = 5;
+	
+	// if( loogikatehe ){ tõene } else { väär }
+	if($age < 18) {
+		echo "alaealine";
+	} else {
+		echo "täisealine";
+	}
+
+
+?>
